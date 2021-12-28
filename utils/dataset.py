2e229efbec64d8970d84fb195eca68141276d92b
song-o <songmi.ohh@gmail.com>
2021-12-28 23:20:30 +0900

    Remove dones, target_v

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,16 +5,14 @@ import os
 class ToMDataset(Dataset):
 
     def __init__(self, episodes, curr_state, target_action, target_prefer=None,
-                 target_sr=None, target_v=None, dones=None, exp='exp1'):
+                 target_sr=None, exp='exp1'):
         self.episodes = episodes
         self.curr_state = curr_state
 
         self.target_action = target_action
         self.target_prefer = target_prefer
         self.target_sr = target_sr
-        self.target_v = target_v
 
-        self.dones = dones
         self.exp = exp
 
     def __len__(self):
@@ -25,7 +23,7 @@ class ToMDataset(Dataset):
             return self.episodes[ind], self.curr_state[ind], self.target_action[ind]
         else:
             return self.episodes[ind], self.curr_state[ind], self.target_action[ind],\
-                   self.target_prefer[ind], self.target_sr[ind], self.target_v[ind], self.dones[ind]
+                   self.target_prefer[ind], self.target_sr[ind]
 
 
 def save_data(npy_data, number, alpha, fn=None):
