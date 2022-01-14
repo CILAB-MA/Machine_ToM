d49ae90c3bf17e69b687331510cbb5dd211e5b8f
song-o <songmi.ohh@gmail.com>
2022-01-14 09:15:42 +0900

    Add action priority

--- agent/reward_seeking_agent.py
+++ agent/reward_seeking_agent.py
@@ -10,15 +10,20 @@ class RewardSeekingAgent():
     action : ['Stay', 'Down', 'Right', 'Up', 'Left']
     '''
 
-    def __init__(self, alpha, num_action=5, move_penalty=-0.01):
-        self.alpha = alpha
-        self.num_action = num_action
-        self.num_object = 4
-        self.reward = np.random.dirichlet(np.full((self.num_object, ), self.alpha))
-        self.discount_factor = 1
-        self.theta = 0.0001
-        self.move_penalty = move_penalty
-        self.most_prefer = np.argmax(self.reward)
+    def __init__(self, alpha, num_action=5, move_penalty=-0.01, act_priority=False):
+            self.alpha = alpha
+            self.num_action = num_action
+            self.num_object = 4
+            self.reward = np.random.dirichlet(np.full((self.num_object, ), self.alpha))
+            self.discount_factor = 1
+            self.theta = 0.0001
+            self.move_penalty = move_penalty
+            self.most_prefer = np.argmax(self.reward)
+            self.act_priority = act_priority
+
+            if self.act_priority == True:
+                self.priority = np.arange(self.num_action)
+                np.random.shuffle(self.priority)
 
     def act(self, observation):
         agent_channel = observation[:, :, -1]
@@ -107,46 +112,125 @@ class RewardSeekingAgent():
         self.policy = np.zeros([self.obs_height, self.obs_width, 5])
         for xi in range(self.obs_height):
             for yi in range(self.obs_width):
+                pris = []
                 A = self._one_step_lookahead(xi, yi, P)
                 best_action = np.argmax(A)
+                if (best_action != 0) and (self.act_priority == True):
+                    action_max = np.where(A == A.max())
+                    for i in action_max[0]:
+                        pris.append(np.where(self.priority == i)[0][0])
+                    best_action = action_max[0][np.argmin(pris)]
                 self.policy[xi, yi, best_action] = 1.0
 
         return self.policy, self.V
 
+def _object_idx_to_color(idx):
+    # objects: ['Blue', 'Pink', 'Orange', 'Green']
+    if idx == 0:
+        return "B"
+    elif idx == 1:
+        return "P"
+    elif idx == 2:
+        return "O"
+    elif idx == 3:
+        return "G"
+
+def _action_idx_to_word(idx):
+    # action: ['Stay', 'Down', 'Right', 'Up', 'Left']
+    if idx == 0:
+        return "Stay"
+    elif idx == 1:
+        return "Down"
+    elif idx == 2:
+        return "Right"
+    elif idx == 3:
+        return "Up"
+    elif idx == 4:
+        return "Left"
+
+def _action_idx_to_one_word(idx):
+    # action: ['Stay', 'Down', 'Right', 'Up', 'Left']
+    if idx == 0:
+        return "S"
+    elif idx == 1:
+        return "D"
+    elif idx == 2:
+        return "R"
+    elif idx == 3:
+        return "U"
+    elif idx == 4:
+        return "L"
 
 if __name__ == '__main__':
-    config = dict(height=11, width=11, pixel_per_grid=8, num_wall=0, preference=100, exp=4, save=True)
-    env = GridWorldEnv(config)
-    print(env.preference)
-    print(env.prefer_reward)
-    obs = env.reset()
-    print('###############################################################')
-    print('############### Check for reward seeking agent  ###############')
-    print('###############################################################')
-    env.obs_well_show()
+    action_priority = True
+    if action_priority == False:
+        config = dict(height=11, width=11, pixel_per_grid=8, num_wall=0, preference=100, exp=4, save=True)
+        env = GridWorldEnv(config)
+        obs = env.reset()
+        print('###############################################################')
+        print('############### Check for reward seeking agent  ###############')
+        print('###############################################################')
+        env.obs_well_show()
 
-    agent = RewardSeekingAgent(alpha=0.01, num_action=5)
-    print('################# Check for agent preference #################')
-    print("agent's preference: ", agent.most_prefer)
-    print("0 : B, 1 : P, 2 : O, 3 : G")
-    agent.train(env.observation)
-    print('############## Check for agent reward function ###############')
-    print(agent.reward)
-    print('################## Check for agent policy ####################')
-    print(np.argmax(agent.policy, axis=2))
-    print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']")
-    print('############## Check for agent value function ################')
-    print(np.around(agent.V.reshape([11, 11]), 2))
+        agent = RewardSeekingAgent(alpha=0.01, num_action=5)
+        print('################# Check for agent preference #################')
+        print("agent's preference: ", agent.most_prefer)
+        print("0 : B, 1 : P, 2 : O, 3 : G")
+        agent.train(env.observation)
+        print('############## Check for agent reward function ###############')
+        print(agent.reward)
+        print('################## Check for agent policy ####################')
+        print(np.argmax(agent.policy, axis=2))
+        print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']")
+        print('############## Check for agent value function ################')
+        print(np.around(agent.V.reshape([11, 11]), 2))
 
-    print('#################### Check for agent act #####################')
-    env.obs_well_show()
-    action = agent.act(env.observation)
-    print(action)
-    print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']")
-    obs, reward, done, _ = env.step(action)
-    env.obs_well_show()
+        print('#################### Check for agent act #####################')
+        env.obs_well_show()
+        action = agent.act(env.observation)
+        print(action)
+        print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']")
+        obs, reward, done, _ = env.step(action)
+        env.obs_well_show()
 
+    else:
+        config = dict(height=11, width=11, pixel_per_grid=8, num_wall=0, preference=100, exp=4, save=True)
+        env = GridWorldEnv(config)
+        obs = env.reset()
+        print('###############################################################')
+        print('############### Check for reward seeking agent  ###############')
+        print('###############################################################')
+        env.obs_well_show()
 
+        agent = RewardSeekingAgent(alpha=0.01, num_action=5, act_priority=True)
+        print('################# Action preference #################')
+        print("agent's action preference: ", agent.priority)
+        pri = []
+        for i in range(agent.num_action):
+            pri.append(_action_idx_to_word(agent.priority[i]))
+        print(pri, "\n")
+
+        print('################# Check for agent preference #################')
+        print("agent's preference: ", agent.most_prefer, _object_idx_to_color(agent.most_prefer), "\n")
+
+        agent.train(env.observation)
+        print('############## Check for agent reward function ###############')
+        print(agent.reward, "\n")
+
+        print('################## Check for agent policy ####################')
+        print(np.argmax(agent.policy, axis=2))
+        print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']", "\n")
+
+        print('############## Check for agent value function ################')
+        print(np.around(agent.V.reshape([11, 11]), 2), "\n")
+
+        print('#################### Check for agent act #####################')
+        env.obs_well_show()
+        action = agent.act(env.observation)
+        print(action)
+        print("action index to text = ['Stay', 'Down', 'Right', 'Up', 'Left']")
+        obs, reward, done, _ = env.step(action)
+        env.obs_well_show()
 
 
 
